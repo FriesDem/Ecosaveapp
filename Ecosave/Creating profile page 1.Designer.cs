7933bcd0728d0b1f89dc1ba35ffaf023b13265ff
Courtney Reid <courtneyreidthesecond@gmail.com>
2022-07-12 13:30:23 -0500

    Not done still need more info from carson

---
+++ Ecosave/Creating profile page 1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Ecosave
+{
+    partial class Creating_profile_page_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Creating_profile_page_1));
+            this.BacklogoBtn = new System.Windows.Forms.PictureBox();
+            this.Backbtn = new System.Windows.Forms.Label();
+            this.MinimizeBtn = new System.Windows.Forms.Label();
+            this.ExitBtn = new System.Windows.Forms.Label();
+            this.NavPanel = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.ProfileBtn = new System.Windows.Forms.Button();
+            this.CalculatorBtn = new System.Windows.Forms.Button();
+            this.TipsBtn = new System.Windows.Forms.Button();
+            this.HomeBtn = new System.Windows.Forms.Button();
+            this.CreateanLbl = new System.Windows.Forms.Label();
+            this.AccountLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.BacklogoBtn)).BeginInit();
+            this.NavPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BacklogoBtn
+            // 
+            this.BacklogoBtn.BackColor = System.Drawing.Color.Transparent;
+            this.BacklogoBtn.Image = ((System.Drawing.Image)(resources.GetObject("BacklogoBtn.Image")));
+            this.BacklogoBtn.Location = new System.Drawing.Point(12, 24);
+            this.BacklogoBtn.Name = "BacklogoBtn";
+            this.BacklogoBtn.Size = new System.Drawing.Size(40, 28);
+            this.BacklogoBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BacklogoBtn.TabIndex = 17;
+            this.BacklogoBtn.TabStop = false;
+            // 
+            // Backbtn
+            // 
+            this.Backbtn.AutoSize = true;
+            this.Backbtn.Font = new System.Drawing.Font("Poppins ExtraLight", 12F);
+            this.Backbtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Backbtn.Location = new System.Drawing.Point(46, 24);
+            this.Backbtn.Name = "Backbtn";
+            this.Backbtn.Size = new System.Drawing.Size(49, 28);
+            this.Backbtn.TabIndex = 16;
+            this.Backbtn.Text = "Back";
+            // 
+            // MinimizeBtn
+            // 
+            this.MinimizeBtn.AllowDrop = true;
+            this.MinimizeBtn.AutoSize = true;
+            this.MinimizeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimizeBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MinimizeBtn.Location = new System.Drawing.Point(1247, 33);
+            this.MinimizeBtn.Name = "MinimizeBtn";
+            this.MinimizeBtn.Size = new System.Drawing.Size(13, 18);
+            this.MinimizeBtn.TabIndex = 19;
+            this.MinimizeBtn.Text = "-";
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.AutoSize = true;
+            this.ExitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ExitBtn.Location = new System.Drawing.Point(1276, 33);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(18, 18);
+            this.ExitBtn.TabIndex = 18;
+            this.ExitBtn.Text = "X";
+            // 
+            // NavPanel
+            // 
+            this.NavPanel.BackColor = System.Drawing.Color.LightCyan;
+            this.NavPanel.Controls.Add(this.pictureBox2);
+            this.NavPanel.Controls.Add(this.ProfileBtn);
+            this.NavPanel.Controls.Add(this.CalculatorBtn);
+            this.NavPanel.Controls.Add(this.TipsBtn);
+            this.NavPanel.Controls.Add(this.HomeBtn);
+            this.NavPanel.Location = new System.Drawing.Point(323, 661);
+            this.NavPanel.Name = "NavPanel";
+            this.NavPanel.Size = new System.Drawing.Size(741, 100);
+            this.NavPanel.TabIndex = 39;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(622, 11);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 49);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 27;
+            this.pictureBox2.TabStop = false;
+            // 
+            // ProfileBtn
+            // 
+            this.ProfileBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ProfileBtn.BackgroundImage")));
+            this.ProfileBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ProfileBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProfileBtn.Location = new System.Drawing.Point(484, 11);
+            this.ProfileBtn.Name = "ProfileBtn";
+            this.ProfileBtn.Size = new System.Drawing.Size(77, 49);
+            this.ProfileBtn.TabIndex = 3;
+            this.ProfileBtn.UseVisualStyleBackColor = true;
+            // 
+            // CalculatorBtn
+            // 
+            this.CalculatorBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("CalculatorBtn.BackgroundImage")));
+            this.CalculatorBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.CalculatorBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CalculatorBtn.Location = new System.Drawing.Point(337, 11);
+            this.CalculatorBtn.Name = "CalculatorBtn";
+            this.CalculatorBtn.Size = new System.Drawing.Size(77, 49);
+            this.CalculatorBtn.TabIndex = 2;
+            this.CalculatorBtn.UseVisualStyleBackColor = true;
+            // 
+            // TipsBtn
+            // 
+            this.TipsBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("TipsBtn.BackgroundImage")));
+            this.TipsBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.TipsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TipsBtn.Location = new System.Drawing.Point(206, 11);
+            this.TipsBtn.Name = "TipsBtn";
+            this.TipsBtn.Size = new System.Drawing.Size(77, 49);
+            this.TipsBtn.TabIndex = 1;
+            this.TipsBtn.UseVisualStyleBackColor = true;
+            // 
+            // HomeBtn
+            // 
+            this.HomeBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("HomeBtn.BackgroundImage")));
+            this.HomeBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.HomeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeBtn.Location = new System.Drawing.Point(73, 11);
+            this.HomeBtn.Name = "HomeBtn";
+            this.HomeBtn.Size = new System.Drawing.Size(77, 49);
+            this.HomeBtn.TabIndex = 0;
+            this.HomeBtn.UseVisualStyleBackColor = true;
+            // 
+            // CreateanLbl
+            // 
+            this.CreateanLbl.AutoSize = true;
+            this.CreateanLbl.Font = new System.Drawing.Font("Poppins ExtraLight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateanLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CreateanLbl.Location = new System.Drawing.Point(43, 96);
+            this.CreateanLbl.Name = "CreateanLbl";
+            this.CreateanLbl.Size = new System.Drawing.Size(152, 48);
+            this.CreateanLbl.TabIndex = 28;
+            this.CreateanLbl.Text = "Setting up";
+            // 
+            // AccountLbl
+            // 
+            this.AccountLbl.AutoSize = true;
+            this.AccountLbl.Font = new System.Drawing.Font("Poppins ExtraLight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AccountLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.AccountLbl.Location = new System.Drawing.Point(43, 138);
+            this.AccountLbl.Name = "AccountLbl";
+            this.AccountLbl.Size = new System.Drawing.Size(196, 48);
+            this.AccountLbl.TabIndex = 40;
+            this.AccountLbl.Text = "Your account";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins ExtraLight", 15F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(292, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 36);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "How many rooms are there?";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Location = new System.Drawing.Point(298, 282);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(251, 140);
+            this.panel2.TabIndex = 42;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(31, 22);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(117, 91);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 39;
+            this.pictureBox3.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(72, 66);
+            this.textBox1.TabIndex = 40;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins ExtraLight", 15F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(714, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(309, 36);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "How many devices are there?";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(732, 282);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(251, 140);
+            this.panel1.TabIndex = 44;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(31, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 39;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 35);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(72, 66);
+            this.textBox2.TabIndex = 41;
+            // 
+            // Creating_profile_page_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(1320, 733);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AccountLbl);
+            this.Controls.Add(this.CreateanLbl);
+            this.Controls.Add(this.NavPanel);
+            this.Controls.Add(this.MinimizeBtn);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.BacklogoBtn);
+            this.Controls.Add(this.Backbtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Creating_profile_page_1";
+            this.Text = "Creating_profile_page_1";
+            ((System.ComponentModel.ISupportInitialize)(this.BacklogoBtn)).EndInit();
+            this.NavPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BacklogoBtn;
+        private System.Windows.Forms.Label Backbtn;
+        private System.Windows.Forms.Label MinimizeBtn;
+        private System.Windows.Forms.Label ExitBtn;
+        private System.Windows.Forms.Panel NavPanel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button ProfileBtn;
+        private System.Windows.Forms.Button CalculatorBtn;
+        private System.Windows.Forms.Button TipsBtn;
+        private System.Windows.Forms.Button HomeBtn;
+        private System.Windows.Forms.Label CreateanLbl;
+        private System.Windows.Forms.Label AccountLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Ecosave/Store Page.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Ecosave
+{
+    partial class Store_Page
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Store_Page));
+            this.NavPanel = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.ProfileBtn = new System.Windows.Forms.Button();
+            this.CalculatorBtn = new System.Windows.Forms.Button();
+            this.TipsBtn = new System.Windows.Forms.Button();
+            this.HomeBtn = new System.Windows.Forms.Button();
+            this.MinimizeBtn = new System.Windows.Forms.Label();
+            this.ExitBtn = new System.Windows.Forms.Label();
+            this.BacklogoBtn = new System.Windows.Forms.PictureBox();
+            this.Backbtn = new System.Windows.Forms.Label();
+            this.NavPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BacklogoBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NavPanel
+            // 
+            this.NavPanel.BackColor = System.Drawing.Color.LightCyan;
+            this.NavPanel.Controls.Add(this.pictureBox2);
+            this.NavPanel.Controls.Add(this.ProfileBtn);
+            this.NavPanel.Controls.Add(this.CalculatorBtn);
+            this.NavPanel.Controls.Add(this.TipsBtn);
+            this.NavPanel.Controls.Add(this.HomeBtn);
+            this.NavPanel.Location = new System.Drawing.Point(323, 665);
+            this.NavPanel.Name = "NavPanel";
+            this.NavPanel.Size = new System.Drawing.Size(741, 100);
+            this.NavPanel.TabIndex = 40;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(622, 11);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 49);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 27;
+            this.pictureBox2.TabStop = false;
+            // 
+            // ProfileBtn
+            // 
+            this.ProfileBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ProfileBtn.BackgroundImage")));
+            this.ProfileBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ProfileBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProfileBtn.Location = new System.Drawing.Point(484, 11);
+            this.ProfileBtn.Name = "ProfileBtn";
+            this.ProfileBtn.Size = new System.Drawing.Size(77, 49);
+            this.ProfileBtn.TabIndex = 3;
+            this.ProfileBtn.UseVisualStyleBackColor = true;
+            // 
+            // CalculatorBtn
+            // 
+            this.CalculatorBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("CalculatorBtn.BackgroundImage")));
+            this.CalculatorBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.CalculatorBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CalculatorBtn.Location = new System.Drawing.Point(337, 11);
+            this.CalculatorBtn.Name = "CalculatorBtn";
+            this.CalculatorBtn.Size = new System.Drawing.Size(77, 49);
+            this.CalculatorBtn.TabIndex = 2;
+            this.CalculatorBtn.UseVisualStyleBackColor = true;
+            // 
+            // TipsBtn
+            // 
+            this.TipsBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("TipsBtn.BackgroundImage")));
+            this.TipsBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.TipsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TipsBtn.Location = new System.Drawing.Point(206, 11);
+            this.TipsBtn.Name = "TipsBtn";
+            this.TipsBtn.Size = new System.Drawing.Size(77, 49);
+            this.TipsBtn.TabIndex = 1;
+            this.TipsBtn.UseVisualStyleBackColor = true;
+            // 
+            // HomeBtn
+            // 
+            this.HomeBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("HomeBtn.BackgroundImage")));
+            this.HomeBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.HomeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeBtn.Location = new System.Drawing.Point(73, 11);
+            this.HomeBtn.Name = "HomeBtn";
+            this.HomeBtn.Size = new System.Drawing.Size(77, 49);
+            this.HomeBtn.TabIndex = 0;
+            this.HomeBtn.UseVisualStyleBackColor = true;
+            // 
+            // MinimizeBtn
+            // 
+            this.MinimizeBtn.AllowDrop = true;
+            this.MinimizeBtn.AutoSize = true;
+            this.MinimizeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimizeBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MinimizeBtn.Location = new System.Drawing.Point(1246, 31);
+            this.MinimizeBtn.Name = "MinimizeBtn";
+            this.MinimizeBtn.Size = new System.Drawing.Size(13, 18);
+            this.MinimizeBtn.TabIndex = 44;
+            this.MinimizeBtn.Text = "-";
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.AutoSize = true;
+            this.ExitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ExitBtn.Location = new System.Drawing.Point(1275, 31);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(18, 18);
+            this.ExitBtn.TabIndex = 43;
+            this.ExitBtn.Text = "X";
+            // 
+            // BacklogoBtn
+            // 
+            this.BacklogoBtn.BackColor = System.Drawing.Color.Transparent;
+            this.BacklogoBtn.Image = ((System.Drawing.Image)(resources.GetObject("BacklogoBtn.Image")));
+            this.BacklogoBtn.Location = new System.Drawing.Point(11, 22);
+            this.BacklogoBtn.Name = "BacklogoBtn";
+            this.BacklogoBtn.Size = new System.Drawing.Size(40, 28);
+            this.BacklogoBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BacklogoBtn.TabIndex = 42;
+            this.BacklogoBtn.TabStop = false;
+            // 
+            // Backbtn
+            // 
+            this.Backbtn.AutoSize = true;
+            this.Backbtn.Font = new System.Drawing.Font("Poppins ExtraLight", 12F);
+            this.Backbtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Backbtn.Location = new System.Drawing.Point(45, 22);
+            this.Backbtn.Name = "Backbtn";
+            this.Backbtn.Size = new System.Drawing.Size(49, 28);
+            this.Backbtn.TabIndex = 41;
+            this.Backbtn.Text = "Back";
+            // 
+            // Store_Page
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1320, 733);
+            this.Controls.Add(this.MinimizeBtn);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.BacklogoBtn);
+            this.Controls.Add(this.Backbtn);
+            this.Controls.Add(this.NavPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Store_Page";
+            this.Text = "Store_Page";
+            this.NavPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BacklogoBtn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel NavPanel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button ProfileBtn;
+        private System.Windows.Forms.Button CalculatorBtn;
+        private System.Windows.Forms.Button TipsBtn;
+        private System.Windows.Forms.Button HomeBtn;
+        private System.Windows.Forms.Label MinimizeBtn;
+        private System.Windows.Forms.Label ExitBtn;
+        private System.Windows.Forms.PictureBox BacklogoBtn;
+        private System.Windows.Forms.Label Backbtn;
+    }
+}
